0a4801c2c0e3c4c4b138e712effaa0638556f472
Nikhil-Dhande <ndhande123@gmail.com>
2024-05-07 22:51:56 +0530

    gen ai update 2

--- src/app/components/Table.Index.js
+++ src/app/components/Table.Index.js
@@ -2309,15 +2309,15 @@ function TableIndex() {
 
 
 
-            <div className="sec m-auto my-10 space-y-8 w-1/2 mob:w-full mob:flex mob:flex-col">
+            <div className="sec m-auto my-10 space-y-8 md:w-1/2 w-full mob:flex mob:flex-col">
                
 
                 <div className="info md:flex md:flex-row  flex flex-col mob:justify-center items-center md:space-y-0 space-y-5 mob:p-5 justify-evenly space-x-3 space-x-0">
-                    <div className="eligibleforswag w-fit mob:w-full h-20 p-5 space-x-5 rounded-lg flex flex-row justify-evenly mob:justify-between items-center bg-green-50 shadow-lg shadow-green-300/30 border border-green-200">
+                    <div className="eligibleforswag md:w-fit w-full h-20 p-5 space-x-5 rounded-lg flex flex-row justify-evenly mob:justify-between items-center bg-green-50 shadow-lg shadow-green-300/30 border border-green-200">
                         <p className="text-center mob:text-start text-sm text-green-400">No of Eligible <br /> Participants for swags</p>
                         <p className="no text-2xl border-l-2 border-l-green-700 pl-3 text-green-800">{EligibleforSwags}</p>
                     </div>
-                    <div className="participationdata w-fit mob:w-full h-20 p-5 space-x-5 rounded-lg flex flex-row justify-evenly mob:justify-between items-center bg-blue-50 shadow-lg shadow-blue-300/30 border border-blue-200">
+                    <div className="participationdata md:w-fit w-full h-20 p-5 space-x-5 rounded-lg flex flex-row justify-evenly mob:justify-between items-center bg-blue-50 shadow-lg shadow-blue-300/30 border border-blue-200">
                         <p className="text-center mob:text-start text-sm text-blue-400">Total No of <br />Participants</p>
                         <p className="no text-2xl border-l-2 border-l-blue-700 pl-3 text-blue-800">{data.length}</p>
                     </div>
